2a0ffcc9e280a98c4550e517a79315e62eb76ed8
Yang He <heyang614.cs@gmail.com>
2021-09-03 01:01:08 -0700

    Delete models directory

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .resnet import get_resnet
-from .resnet_cifar import get_cifar_resnet
-
-def get_classification_model(arch, pretrained, **kwargs):
-    return get_resnet(arch, pretrained, **kwargs)
-
-def get_cifar_classification_model(arch, pretrained, **kwargs):
-    return get_cifar_resnet(arch, pretrained, **kwargs)
